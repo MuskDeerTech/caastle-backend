0b093799c641fc57f22e405ec2447458046e66fd
MuskDeerTech <muskdeertechnologies@gmail.com>
2025-08-07 18:20:47 +0530

    embedding

--- utils/embedding.js
+++ utils/embedding.js
@@ -1,21 +1,25 @@
 // utils/embedding.js
+
 const use = require('@tensorflow-models/universal-sentence-encoder');
-const tf = require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs'); // use this, not tfjs-node
 
-let model = null;
+let model;
 
-async function loadModel() {
+const loadModel = async () => {
   if (!model) {
     model = await use.load();
+    console.log("✅ USE model loaded (tfjs)");
   }
   return model;
-}
+};
 
-async function getEmbedding(text) {
+const getEmbedding = async (text) => {
   const model = await loadModel();
   const embeddings = await model.embed([text]);
   const array = await embeddings.array();
-  return array[0];
-}
+  return array[0]; // return single vector
+};
 
-module.exports = { getEmbedding };
+module.exports = {
+  getEmbedding,
+};
